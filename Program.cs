36c516bce9006349385fc535b5aece7037fe5b69
DavidE02 <47436519+DavidE02@users.noreply.github.com>
2023-11-16 11:36:03 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main class
+    internal class Program
+    {
+        // Entry point method
+        static void Main(string[] args)
+        {
+            // SET app variables
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "David Estrada";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset text color
+            Console.ResetColor();
+
+            // Asks users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", input);
+        }
+    }
+}
